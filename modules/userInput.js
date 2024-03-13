15d099e9bb25ba1041465afc6f52c0f981db6a8a
arpdkvcs <arpdkvcs@gmail.com>
2024-03-13 03:48:22 +0100

    Change limit of generated credentials

--- modules/userInput.js
+++ modules/userInput.js
@@ -8,7 +8,7 @@ const readlineSync = pkg;
  */
 export function getNumberOfCredentials() {
   const min = 1;
-  const max = 10000;
+  const max = 100000;
   const prompt =
     `\nEnter the number of username/password combinations` +
     `\nto generate (between ${min} and ${max}): `;
